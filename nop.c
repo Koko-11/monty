2f52966331aaa5e71369b78ea302fab9a529c7dc
Koko-11 <idara.koko@gmail.com>
2024-09-26 10:56:24 -0700

    .

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer
+ * @line_number: Int
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
---
+++ swap.c
@@ -0,0 +1,20 @@
+/**
+ * swap_nodes - Swaps the top two elements of the stack.
+ * @stack: Pointer
+ * @line_number: int
+ */
+void swap_nodes(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "swap");
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	tmp->prev = NULL;
+	*stack = tmp;
+}
