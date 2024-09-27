22b332b1c8eca12b3c3b186aacfdd4252344dbb9
Koko-11 <idara.koko@gmail.com>
2024-09-26 22:17:00 -0700

    .

--- monty1.c
+++ monty1.c
@@ -1,157 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-#define EXIT_FAILURE 1
+#include "monty.h"
 
 /**
- * struct stack_s - Structure representing a stack node.
- * @n: Integer data stored in the node.
- * @next: Pointer to the next node in the stack.
+ * err - Prints appropiate error messages determined by their error code.
+ * @error_code: The error codes are the following:
+ * (1) => The user does not give any file or more than one file to the program.
+ * (2) => The file provided is not a file that can be opened or read.
+ * (3) => The file provided contains an invalid instruction.
+ * (4) => When the program is unable to malloc more memory.
+ * (5) => When the parameter passed to the instruction "push" is not an int.
+ * (6) => When the stack it empty for pint.
+ * (7) => When the stack it empty for pop.
+ * (8) => When stack is too short for operation.
  */
-typedef struct stack_s
+void err(int error_code, ...)
 {
-    int n;
-    struct stack_s *next;
-} stack_t;
+	va_list ag;
+	char *op;
+	int l_num;
 
-stack_t *stack = NULL; // Global stack
-
-/**
- * is_integer - Check if a string represents a valid integer.
- * @str: The string to check.
- * Return: 1 if it's a valid integer, 0 otherwise.
- */
-int is_integer(const char *str)
-{
-    if (str == NULL || *str == '\0')
-        return 0;
-
-    if (*str == '-' || *str == '+')  // Check for negative or positive sign
-        str++;
-
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-
-    return 1;
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
- * push - Pushes an integer onto the stack.
- * @line_number: The current line number in the file (for error reporting).
- * @value: The value to push onto the stack.
+ * more_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (6) => When the stack it empty for pint.
+ * (7) => When the stack it empty for pop.
+ * (8) => When stack is too short for operation.
+ * (9) => Division by zero.
  */
-void push(int line_number, char *value)
+void more_err(int error_code, ...)
 {
-    if (!is_integer(value))
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	va_list ag;
+	char *op;
+	int l_num;
 
-    int num = atoi(value);
-    stack_t *new_node = malloc(sizeof(stack_t));
-
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = num;
-    new_node->next = stack; // Insert the new node at the top
-    stack = new_node; // Update the stack to point to the new top
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(ag, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n",
+				va_arg(ag, unsigned int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
- * pall - Prints all values in the stack from top to bottom.
+ * string_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (10) ~> The number inside a node is outside ASCII bounds.
+ * (11) ~> The stack is empty.
  */
-void pall(void)
+void string_err(int error_code, ...)
 {
-    stack_t *current = stack;
+	va_list ag;
+	int l_num;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
-
-/**
- * free_stack - Frees the memory used by the stack.
- */
-void free_stack(void)
-{
-    stack_t *current = stack;
-
-    while (current != NULL)
-    {
-        stack_t *next_node = current->next;
-        free(current);
-        current = next_node;
-    }
-}
-
-/**
- * main - Entry point of the program.
- * @argc: The number of arguments passed to the program.
- * @argv: Array of argument strings.
- * Return: 0 on success, EXIT_FAILURE on error.
- */
-int main(int argc, char *argv[])
-{
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int line_number = 0;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        line_number++;
-        char *opcode = strtok(line, " \n\t");
-
-        if (opcode == NULL || opcode[0] == '#')  // Ignore empty lines or comments
-            continue;
-
-        if (strcmp(opcode, "push") == 0)
-        {
-            char *arg = strtok(NULL, " \n\t");
-            push(line_number, arg);
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall();
-        }
-        else
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-            free(line);
-            fclose(file);
-            free_stack();
-            return EXIT_FAILURE;
-        }
-    }
-
-    free(line);
-    fclose(file);
-    free_stack();
-    return 0;
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
